5a3c79c8e53df3b8160305efdf8c5b2aa97e8549
Ozan Uslan <github@ozanuslan.com>
2023-12-21 22:50:43 +0300

    test: marshall working?

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
@@ -39,37 +40,67 @@ func NewDNSHeader() *DNSHeader {
 	}
 }
 
-func (h *DNSHeader) MarshallBytes(bytes []byte) {
-	h.ID = uint16(bytes[0])<<8 | uint16(bytes[1])
-	h.Response = bytes[2]&0x80 != 0
-	h.Opcode = (bytes[2] >> 3) & 0x0F
-	h.Authoritative = bytes[2]&0x04 != 0
-	h.Truncated = bytes[2]&0x02 != 0
-	h.RecursionDesired = bytes[2]&0x01 != 0
-	h.RecursionAvailable = bytes[3]&0x80 != 0
-	h.Reserved = (bytes[3] >> 4) & 0x07
-	h.ResponseCode = bytes[3] & 0x0F
-	h.QuestionCount = uint16(bytes[4])<<8 | uint16(bytes[5])
-	h.AnswerCount = uint16(bytes[6])<<8 | uint16(bytes[7])
-	h.AuthorityCount = uint16(bytes[8])<<8 | uint16(bytes[9])
-	h.AdditionalCount = uint16(bytes[10])<<8 | uint16(bytes[11])
+func UnmarshalDNSHeader(data []byte) (*DNSHeader, error) {
+	if len(data) < 12 {
+		return nil, fmt.Errorf("insufficient data to unmarshal DNS header")
+	}
+
+	header := &DNSHeader{}
+
+	// Unmarshal fields using binary.BigEndian
+	header.ID = binary.BigEndian.Uint16(data[:2])
+	header.Response = (data[2] & 0x80) != 0
+	header.Opcode = (data[2] >> 3) & 0x0F
+	header.Authoritative = (data[2] & 0x04) != 0
+	header.Truncated = (data[2] & 0x02) != 0
+	header.RecursionDesired = (data[2] & 0x01) != 0
+	header.RecursionAvailable = (data[3] & 0x80) != 0
+	header.Reserved = (data[3] >> 4) & 0x07
+	header.ResponseCode = data[3] & 0x0F
+	header.QuestionCount = binary.BigEndian.Uint16(data[4:6])
+	header.AnswerCount = binary.BigEndian.Uint16(data[6:8])
+	header.AuthorityCount = binary.BigEndian.Uint16(data[8:10])
+	header.AdditionalCount = binary.BigEndian.Uint16(data[10:12])
+
+	return header, nil
 }
 
-func (h *DNSHeader) UnmarshallBytes() []byte {
-	bytes := make([]byte, 12)
-	bytes[0] = byte(h.ID >> 8)
-	bytes[1] = byte(h.ID)
-	bytes[2] = byte(h.Opcode<<3) | byte(h.ResponseCode)
-	bytes[3] = byte(h.ResponseCode)
-	bytes[4] = byte(h.QuestionCount >> 8)
-	bytes[5] = byte(h.QuestionCount)
-	bytes[6] = byte(h.AnswerCount >> 8)
-	bytes[7] = byte(h.AnswerCount)
-	bytes[8] = byte(h.AuthorityCount >> 8)
-	bytes[9] = byte(h.AuthorityCount)
-	bytes[10] = byte(h.AdditionalCount >> 8)
-	bytes[11] = byte(h.AdditionalCount)
-	return bytes
+func MarshalDNSHeader(header *DNSHeader) ([]byte, error) {
+	data := make([]byte, 12)
+
+	// Marshal fields using binary.BigEndian
+	binary.BigEndian.PutUint16(data[:2], header.ID)
+
+	flags := uint8(0)
+	if header.Response {
+		flags |= 0x80
+	}
+	flags |= (header.Opcode & 0x0F) << 3
+	if header.Authoritative {
+		flags |= 0x04
+	}
+	if header.Truncated {
+		flags |= 0x02
+	}
+	if header.RecursionDesired {
+		flags |= 0x01
+	}
+	data[2] = flags
+
+	flags2 := uint8(0)
+	if header.RecursionAvailable {
+		flags2 |= 0x80
+	}
+	flags2 |= (header.Reserved & 0x07) << 4
+	flags2 |= header.ResponseCode & 0x0F
+	data[3] = flags2
+
+	binary.BigEndian.PutUint16(data[4:6], header.QuestionCount)
+	binary.BigEndian.PutUint16(data[6:8], header.AnswerCount)
+	binary.BigEndian.PutUint16(data[8:10], header.AuthorityCount)
+	binary.BigEndian.PutUint16(data[10:12], header.AdditionalCount)
+
+	return data, nil
 }
 
 type DNSMessage struct {
@@ -104,11 +135,20 @@ func main() {
 
 		fmt.Printf("Received %d bytes from %s\n", n, remoteAddr)
 
-		header := NewDNSHeader()
-		header.MarshallBytes(buffer[:12])
+		header, err := UnmarshalDNSHeader(buffer[:12])
+		if err != nil {
+			fmt.Println("Error unmarshalling DNS header:", err)
+			return
+		}
+
 		header.Response = true
 
-		responseMsg := header.UnmarshallBytes()
+		responseMsg, err := MarshalDNSHeader(header)
+		if err != nil {
+			fmt.Println("Error marshalling DNS header:", err)
+			return
+		}
+		fmt.Printf("Message bits %b\n", responseMsg)
 		conn.WriteMsgUDP(responseMsg, make([]byte, 0), remoteAddr)
 	}
 }
